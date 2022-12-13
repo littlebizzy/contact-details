4d1922ce20111ce5e335ac7fa55c04c3539430bb
Jesse Nickles <jesse@littlebizzy.com>
2022-12-14 02:34:29 +0700

    Update contact-information.php

--- contact-information.php
+++ contact-information.php
@@ -1,1 +1,10 @@
 <?php 
+/*
+Plugin Name: Contact Information
+Plugin URI: 
+Description:
+Version: 0.0.0
+// Text Domain: contact-info
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+*/     
