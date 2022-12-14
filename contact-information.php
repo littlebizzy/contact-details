2ecf1e755d80643e30cd7b1d5cf22355d7cc8112
Jesse Nickles <jesse@littlebizzy.com>
2022-12-15 01:32:13 +0700

    Update contact-information.php

--- contact-information.php
+++ contact-information.php
@@ -2,9 +2,11 @@
 /*
 Plugin Name: Contact Information
 Plugin URI: 
+GitHub Plugin URI: littlebizzy/contact-information
 Description:
 Version: 0.0.0
-// Text Domain: contact-info
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */     
