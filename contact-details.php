16d69e2a5529164dc80afdc15c61567b63258b4b
Jesse Nickles <jesse@littlebizzy.com>
2022-12-15 02:47:18 +0700

    Update contact-details.php

--- contact-details.php
+++ contact-details.php
@@ -9,4 +9,11 @@ Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
-*/     
+Forked from: https://wordpress.org/plugins/contact-details/
+*/
+
+// ensure class defined
+if( !class_exists( 'LittleBizzy_Contact_Details' ) )
+require_once( trailingslashit( dirname( __FILE__ ) ) . 'class.contact-details.php' );
+
+new LittleBizzy_Contact_Details();
